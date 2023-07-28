03aecab5fec795c757dbf52dc47231b5c6d4c45b
Le Tri <leminhtri151102@gmail.com>
2023-07-28 17:55:32 +0700

    update code

--- lib/dice_roll.dart
+++ lib/dice_roll.dart
@@ -16,7 +16,7 @@ class _DiceRollState extends State<DiceRoll> {
   var changeDice1 = 'assets/images/dice-1.png';
   var changeDice2 = 'assets/images/dice-1.png';
   var changeDice3 = 'assets/images/dice-1.png';
-  
+
   String resultDiv = '';
   int sum = 0;
 
@@ -40,88 +40,90 @@ class _DiceRollState extends State<DiceRoll> {
       resultDiv = 'Odd';
     }
 
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            Column(
-              children: [
-                Text(
-                  dicePri1.toString(),
-                  style: const TextStyle(color: Colors.white, fontSize: 25),
-                ),
-                Image.asset(
-                  changeDice1,
-                  width: 120,
-                ),
-              ],
-            ),
-            Column(
-              children: [
-                Text(
-                  dicePri2.toString(),
-                  style: const TextStyle(color: Colors.white, fontSize: 25),
-                ),
-                Image.asset(
-                  changeDice2,
-                  width: 120,
-                ),
-              ],
-            ),
-            Column(
-              children: [
-                Text(
-                  dicePri3.toString(),
-                  style: const TextStyle(color: Colors.white, fontSize: 25),
-                ),
-                Image.asset(
-                  changeDice3,
-                  width: 120,
-                ),
-              ],
-            ),
-          ],
-        ),
-        const SizedBox(height: 30),
-        TextButton(
-          onPressed: rollDice,
-          style: TextButton.styleFrom(
-            foregroundColor: Colors.black,
-            backgroundColor: Colors.white,
-            textStyle: const TextStyle(
-              fontSize: 25,
-              fontWeight: FontWeight.w600,
-            ),
+    return SingleChildScrollView(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Column(
+                children: [
+                  Text(
+                    dicePri1.toString(),
+                    style: const TextStyle(color: Colors.white, fontSize: 25),
+                  ),
+                  Image.asset(
+                    changeDice1,
+                    width: 120,
+                  ),
+                ],
+              ),
+              Column(
+                children: [
+                  Text(
+                    dicePri2.toString(),
+                    style: const TextStyle(color: Colors.white, fontSize: 25),
+                  ),
+                  Image.asset(
+                    changeDice2,
+                    width: 120,
+                  ),
+                ],
+              ),
+              Column(
+                children: [
+                  Text(
+                    dicePri3.toString(),
+                    style: const TextStyle(color: Colors.white, fontSize: 25),
+                  ),
+                  Image.asset(
+                    changeDice3,
+                    width: 120,
+                  ),
+                ],
+              ),
+            ],
           ),
-          child: const Text(' Roll Dice '),
-        ),
-        const SizedBox(height: 50),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Text(
-              'Total: $sum',
-              style: const TextStyle(
-                fontSize: 20,
-                color: Colors.white,
+          const SizedBox(height: 50),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Text(
+                'Total: $sum',
+                style: const TextStyle(
+                  fontSize: 30,
+                  color: Colors.white,
+                ),
+              ),
+              Row(
+                children: [
+                  Text(
+                    'Parity: $resultDiv',
+                    style: const TextStyle(
+                      fontSize: 30,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          const SizedBox(height: 100),
+          TextButton(
+            onPressed: rollDice,
+            style: TextButton.styleFrom(
+              foregroundColor: Colors.black,
+              backgroundColor: Colors.white,
+              textStyle: const TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.w600,
               ),
             ),
-            Row(
-              children: [
-                Text(
-                  'Parity: $resultDiv',
-                  style: const TextStyle(
-                    fontSize: 20,
-                    color: Colors.white,
-                  ),
-                ),
-              ],
-            ),
-          ],
-        ),
-      ],
+            child: const Text(' Roll Dice '),
+          ),
+        ],
+      ),
     );
   }
 }
